057e7823a4fb7fa33e3c3e92a0fa07ca235451aa
Michael <micjejim@espol.edu.ec>
2025-10-16 16:04:30 -0500

    implementar get tareas

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,12 @@ app.use(express.json());
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
-    
+let tasks = [
+    { id: 1, title: 'Aprender Git', done: false },
+    { id: 2, title: 'Hacer el workshop', done: false }
+];
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+  
