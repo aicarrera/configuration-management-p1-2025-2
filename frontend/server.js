5f154ee209734f0cc0f6c56add3203ede7d47eb1
Tburgos11 <109999494+Tburgos11@users.noreply.github.com>
2025-10-16 16:07:56 -0500

    post

---
+++ frontend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.listen(port, () => {
+    console.log(`Servidor frontend escuchando en http://localhost:${port}`);
+});
